bb936c7c104e471c4650fddc3888b261365b024a
nbilog <nbilog@ucsd.edu>
2022-05-01 15:25:29 -0700

    Update practicetester.java

--- practicetester.java
+++ practicetester.java
@@ -6,6 +6,6 @@ public class practicetester {
 
     @Test
     public void subtraction(){
-        assertEquals(2, practice.subtract(3,5)); //a test that fails initially
+        assertEquals(2, practice.subtract(4,2)); //a test that fails initially
     }
 }
