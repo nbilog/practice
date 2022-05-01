38e98446aca1d1c92ae1eb82bed3f0fdcc43eb1a
nbilog <nbilog@ucsd.edu>
2022-05-01 15:14:32 -0700

    create test files

--- practice.java
+++ practice.java
@@ -1,4 +1,7 @@
 public class practice{
 
+    public static int subtract(int a, int b){
+        return a - b;
+    }
 
 }
---
+++ practicetester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class practicetester {
+
+    @Test
+    public void subtraction(){
+        assertEquals(2, practice.subtract(3,5)); //a test that fails initially
+    }
+}
